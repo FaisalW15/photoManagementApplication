926e977bba6232f6ab7b16943ae8b05b4246b586
Faisal15 <alwazzan.faisal1@gmail.com>
2025-04-12 12:41:13 +0300

    added classes from project file

---
+++ photoManagementApplication/src/photoManagementApplication/photoManager.java
@@ -0,0 +1,12 @@
+package photoManagementApplication;
+
+public class photoManager {
+	// Constructor
+	public PhotoManager();
+	// Return all managed photos
+	public LinkedList<Photo> getPhotos();
+	// Add a photo
+	public void addPhoto(Photo p);
+	// Delete a photo
+	public void deletePhoto(String path);
+	}
