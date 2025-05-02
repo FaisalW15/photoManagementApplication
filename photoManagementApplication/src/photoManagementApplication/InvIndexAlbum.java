1ec2ad0288388bb0f49717332277d552483e0b64
Faisal15 <alwazzan.faisal1@gmail.com>
2025-05-02 17:23:01 +0300

    .

--- photoManagementApplication/src/photoManagementApplication/InvIndexAlbum.java
+++ photoManagementApplication/src/photoManagementApplication/InvIndexAlbum.java
@@ -1,5 +1,18 @@
 package photoManagementApplication;
 
 public class InvIndexAlbum {
+	
+	String name;
+	String condition;
+	InvIndexPhotoManager invIndexPhotoManager;
+	
+	
+	public InvIndexAlbum(String name, String condition, InvIndexPhotoManager invINdexPhotoManager) {
+		
+		this.name = name;
+		this.condition = condition;
+		this.invIndexPhotoManager = invIndexPhotoManager;
+		
+	}
 
 }
