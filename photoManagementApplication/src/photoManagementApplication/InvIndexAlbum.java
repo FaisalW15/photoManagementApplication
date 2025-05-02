b422f22383b299ff014f51967d07e63468a13100
Faisal15 <alwazzan.faisal1@gmail.com>
2025-05-02 17:42:04 +0300

    isPhotoInList method implemented, will be needed for future methods

--- photoManagementApplication/src/photoManagementApplication/InvIndexAlbum.java
+++ photoManagementApplication/src/photoManagementApplication/InvIndexAlbum.java
@@ -14,5 +14,43 @@ public class InvIndexAlbum {
 		this.invIndexPhotoManager = invIndexPhotoManager;
 		
 	}
+	
+	//return name of album
+	public String getname() {
+		return name;
+	}
+	
+	//return condition associated with the album
+	public String getCondition() {
+		return condition;
+	}
+
+    //return the manager
+	public InvIndexPhotoManager getManager() {
+		return invIndexPhotoManager;
+	}
+	
+	
+	//checks if a given photo is in the given linkedList
+	public boolean isPhotoInList(LinkedList<Photo> LL, Photo photo) {
+		
+		if(LL.empty()) {
+			return false;
+		}
+		
+		LL.FindFirst();
+		while(!(LL.last())) {
+			if(LL.retrieve().path.equals(photo.path)) {
+				return true;
+			}
+		LL.findNext();
+		}
+		
+		//check last node
+		if(LL.retrieve().path.equals(photo.path)) {
+			return true;
+		}
+		return false;
+	}
 
 }
